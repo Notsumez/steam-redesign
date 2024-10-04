392916af018be37374dfed8d4e0499a8385cc04e
Notsumez <adahorapirola@gmail.com>
2024-10-04 18:43:39 -0300

    Preparação do Ambiente

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, sit, ut vero eius cupiditate iste quibusdam porro excepturi tenetur modi ipsum animi odio impedit perspiciatis hic non ex unde quis!</h1>
+    </div>
+  );
+}
